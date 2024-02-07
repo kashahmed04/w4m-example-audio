7d3c49d282b8e515079a9cb80a672c524534d79a
Kashaf Ahmed <kka7705@g.rit.edu>
2024-02-07 10:44:30 -0500

    Updated

--- src/main.ts
+++ src/main.ts
@@ -214,8 +214,146 @@ tenorAudio.addEventListener('ended', () => {
 
   we are going to look at 3 different audios today 
 
-  BRANCH 4B:
+  AUDIO 4B:
 
   we go to current branch tab to change bracnhes 
+  theres a scroll bar the same as audio 4 but we add the visualization for the tone of voice
+  we have 4 divs and each of tem have 1024 spans with 100 pixels wide and tall**
+  and we change each span based on the audio we get back**
+
+  part has new class properties and its call a pass through since it inspects the audio and does not change it**
+  we also have a uint 8 array and its an unsigned integer 8 bit and its different from typical JS array because its a fixed size and we cant 
+  add or remove from it**
+  since its a fixed size its easier to add new frames since there will be no data changed**
+
+  we have pur array of span element (bars)** 
+
+  and we have markup array which is parent of the bars**
+
+  audio context to set the analyzer (it stands out for FFT) and FFT is an algorithm for taking wave form signal and making audios into frequencies**
+  we also use the transform property to scale a picture as a percetange (usually use percentqge with transform)(fast because its a compisition element)**
+
+  we have the fftsize which is the 1024 samples but we always put double for the fftsize to get it out (has to be powers of 2 numbers though no odd nubers)**
+
+  frequency bin count does**
+
+  make the div and set the height then we expand visualize and it generates a bar and puts it in bars array
+  for the fft array and appends it to visualziermarkup and we do request animtation frame to**
+
+  anyimte we pass a function as an event handler its going to forget the this argument for the function and we use an arrow function to bind the this.
+  to the fucntion without saying this. while making a function**
+
+  analyzernode has the fft arra and it automatically**
+
+  iterate through array and calulate amplitude and the data is going to range from 0-255 but we change it from 0-100 because**
+
+  then we update stlye stranfrom for that bars for the scaleY to be**
+
+  requestanimation frame to update per frame for this loop**
+
+  TIME DOMAIN DATA (us this a differnet example):
+
+  more of the raw wave form rather than frequency distribution like previous exmaple
+
+  in styles we have a bar and all of our spans have a bar**
+
+  we set it as inline block and have it line up next to its neighbor and have tranform origin be where we make the origin to rotate it at the bottom
+  left so it roates on the bottom left**
+
+  AUDIO 5 (POSITIONAL AUDIO):
+
+  we have a dragger JS file and we have the circle and the label and the coorindates with an x,y,z attached to it and we have a boolean for dragging state
+  and we have an offset which**
+
+  we have a position callback which takes in coordinates and returns nothing and we call it to update parts of the app as we move on**
+
+  we make the elements and set the coorindates and apply them and we have mousedoen element and set the dragging to true and the x and y offset based on our mouse**
+
+  we have a mousemove event and if its dragging we update its coordinates and it takes our mouse coorindates and subtracts it from our offset so we get back
+  to the same point we clicked on**
+
+  there is a register update function that lets something else specify the callback and we call the position callback and when we apply coorindates we update
+  the coorindates and the**
+
+  in the main javascript file we have starting coorindates for our obkects and make a new dragger for all the drags in the document
+
+  in index.html we have 5 divs for our audios and our ears**
+
+  we do query selector to get**
+
+  we are going to query selector all to get a div element for our element (returns a div)**
+
+  we push it into our draggers array and if we give the foreach a second argument then**
+
+  we give the draggers to the part objects and we have a listener and it has a position and a forward value**
+
+  0,0 is in the upper left y is positve on the bottom and x is positive right and z is positive into the screen and negative outside of screen**
+
+  listenders forward value is straight up and the up value is pointing outside of the screen**
+
+  we update the label to the ears and every time it updates its position it takes coorindates and applies it to position of the listener**
+
+  for the part class we have the coorindates and we have a panner node and the dragger and we update the label and when we make 
+  panner node it has 14 different properties and we make it an object when we declare our new apnner node instance**
+
+  panning model is**
+
+  the diatnce model is the decay and we use linear**
+
+  we have the position for the audio source in the scene and the orientation which is what direction its facing**
+
+  we have ref distance which is how we make the scale of our scene and models and we leave it as one because**
+
+  we have max distance is the units we have before we stop hearing something and the rollofffactor is how fast it cuts off while we get further away**
+
+  the cone inner angle is**
+
+  the cone outer gain is everything outside of the cone and**
+
+  for the patch we have our source node and our gain node and we connect the panner node so we get spatial effects as well**
+
+  whatever audio we click on the compare it compres the file to the previous one we had seen**
+
+  AUDIO 6 (ANIMATION):
+
+  we took the wave output from the 4B and apply it to the transform to the upper half of the character and since the roatation is first then**
+
+  most of the changes are in dragger and we has to lower (chin) and upper is the head and eyebrows and the eyes are in the style sheet**
+
+  in the upper we have eyes and we have peusdeo selector and it lets us specify**
+
+  we say the content is display block with width and height with**
+
+  eyes before and eyes after have specifial conditons set to it**
+
+  in dragger we have making the character in a lot of elements we create in divs**
+
+  we have a sing function and we put the rotation and last percent within the file but we should put it at the top of the class file for properties and vairbales**
+
+  we get a percent and the first conditon says take a look at current percent and compare it to last percent and change the orientation of the character
+
+  and the second condtional is when we start from 0 and get a higher percent (quiet and started singing again) so set roation
+
+  the last constional is when we are silent close the mouth (roation)**
+
+  the position of the eyes brows are tied to roation and we have the roatin (-15 to 15 then get divide by 15 then take absolute value
+  then we apply it as a translate onto the eyebrow and multiply by 3 and 5 for one eyebrow to have them slightly offset)**
+
+  when we deal with transform the order of operations matter (translate.vs rotate and rotate.vs. translte)
+
+  we save the percent so then we are ready for the next frame for audio calculations**
+
+  we use transform because its compsoitioning and its better for the browser to handle**
+
+  in part we create analyzer and use fft size of 32 beause we are not visualizing whole frequency and the resulting data arry then has 
+  16 elements in it**
+
+  panner things are still the same as file 5 and measure audio is called with request animation frame 
+
+  we take the data at postiion 8 at divide it by 255 to make it since (we chose smaple 8 because it looked the most interesting when it played)**
+
+  
+
+
 
 */
